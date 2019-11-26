e149234d252388a520ebe7bdc3fe18f8785eb014
Vysakh Sreenivasan <vysakh@pipesort.com>
2019-11-26 16:44:25 +0530

    On the 5th click authenticate the user to a video call

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 import Button from './Button';
 
 interface AuthProps {
-  heading?: string
+  heading?: string,
+  authenticate: (isHuman: boolean) => void
 }
 
-const Auth: React.FC<AuthProps> = ({ heading }) => {
+const Auth: React.FC<AuthProps> = ({ heading, authenticate }) => {
   const [count, setCount] = useState(5)
+  const handleClick = () => {
+    // count === 1 && authenticate(true)
+    if (count === 1) authenticate(true)
+
+    setCount(count - 1)
+  }
   return (
     <div className="Auth">
       <h2>{heading}</h2>
       <p>Press the "I'm human" button {count} times</p>
-      <Button onClick={() => setCount(count - 1)} data-testid="increment"> I'm human </Button>
+      <Button onClick={handleClick} > I'm human </Button>
     </div>
   );
 }
